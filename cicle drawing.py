e3631abce608171e54dad2cab3e6dc212ca7e270
Aron <62036603+Aronace21@users.noreply.github.com>
2020-05-10 15:37:16 -0400

    Add files via upload

---
+++ cicle drawing.py
@@ -0,0 +1,11 @@
+import turtle
+
+window = turtle.Screen()
+
+turtle.speed(10)
+turtle.bgcolor("black") 
+turtle.color("cyan" , "green")
+
+turtle.begin_fill()
+turtle.circle(196)
+turtle.end_fill()
